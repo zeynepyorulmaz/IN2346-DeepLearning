de9714d0ace354d4d541f0e412d5ece0c7899f0e
Zeynep Yorulmaz <109173753+zeynepyorulmaz@users.noreply.github.com>
2025-06-04 16:07:08 +0200

    added exercise 5

---
+++ exercise_05/exercise_code/networks/layer.py
@@ -0,0 +1,179 @@
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        """
+        :param x: Inputs, of any shape.
+
+        :return out: Outputs, of the same shape as x.
+        :return cache: Cache, stored for backward computation, of the same shape as x.
+        """
+        shape = x.shape
+        out, cache = np.zeros(shape), np.zeros(shape)
+        ########################################################################
+        # TODO:                                                                #
+        # Implement the forward pass of Sigmoid activation function            #
+        ########################################################################
+
+        # Sigmoid function: 1 / (1 + exp(-x))
+        out = 1.0 / (1.0 + np.exp(-x))
+        cache = out  # Store output for backward pass
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return out, cache
+
+    def backward(self, dout, cache):
+        """
+        :param dout: Upstream gradient from the computational graph, from the Loss function
+                    and up to this layer. Has the shape of the output of forward().
+        :param cache: The values that were stored during forward() to the memory,
+                    to be used during the backpropogation.
+        :return: dx: the gradient w.r.t. input X, of the same shape as X
+        """
+        dx = None
+        ########################################################################
+        # TODO:                                                                #
+        # Implement the backward pass of Sigmoid activation function           #
+        ########################################################################
+
+        # Derivative of sigmoid: sigmoid(x) * (1 - sigmoid(x))
+        # cache contains the output of sigmoid from forward pass
+        sigmoid_out = cache
+        dx = dout * sigmoid_out * (1 - sigmoid_out)
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return dx
+
+
+class Relu:
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        """
+        :param x: Inputs, of any shape.
+
+        :return outputs: Outputs, of the same shape as x.
+        :return cache: Cache, stored for backward computation, of the same shape as x.
+        """
+        out = None
+        cache = None
+        ########################################################################
+        # TODO:                                                                #
+        # Implement the forward pass of Relu activation function               #
+        ########################################################################
+
+        # ReLU function: max(0, x)
+        out = np.maximum(0, x)
+        cache = x  # Store input for backward pass
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return out, cache
+
+    def backward(self, dout, cache):
+        """
+        :param dout: Upstream gradient from the computational graph, from the Loss function
+                    and up to this layer. Has the shape of the output of forward().
+        :param cache: The values that were stored during forward() to the memory,
+                    to be used during the backpropogation.
+        :return: dx: the gradient w.r.t. input X, of the same shape as X
+        """
+        dx = None
+        ########################################################################
+        # TODO:                                                                #
+        # Implement the backward pass of Relu activation function              #
+        ########################################################################
+
+        # Derivative of ReLU: 1 if x > 0, else 0
+        # cache contains the input from forward pass
+        x = cache
+        dx = dout * (x > 0)
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return dx
+
+
+def affine_forward(x, w, b):
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
+    Inputs:
+    :param x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    :param w: A numpy array of weights, of shape (D, M)
+    :param b: A numpy array of biases, of shape (M,)
+    :return out: output, of shape (N, M)
+    :return cache: (x, w, b)
+    """
+    N, M = x.shape[0], b.shape[0]
+    out = np.zeros((N,M))
+    ########################################################################
+    # TODO: Implement the affine forward pass. Store the result in out.    #
+    # You will need to reshape the input into rows.                        #
+    ########################################################################
+
+    # Reshape input from (N, d_1, ..., d_k) to (N, D) where D = d_1 * ... * d_k
+    x_reshaped = x.reshape(N, -1)
+    
+    # Affine transformation: out = x * w + b
+    out = x_reshaped.dot(w) + b
+
+    ########################################################################
+    #                           END OF YOUR CODE                           #
+    ########################################################################
+    cache = (x, w, b)
+    return out, cache
+
+
+def affine_backward(dout, cache):
+    """
+    Computes the backward pass for an affine layer.
+    Inputs:
+    :param dout: Upstream derivative, of shape (N, M)
+    :param cache: Tuple of:
+      - x: Input data, of shape (N, d_1, ... d_k)
+      - w: Weights, of shape (D, M)
+      - b: A numpy array of biases, of shape (M,
+    :return dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    :return dw: Gradient with respect to w, of shape (D, M)
+    :return db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, b = cache
+    dx, dw, db = None, None, None
+    ########################################################################
+    # TODO: Implement the affine backward pass.                            #
+    ########################################################################
+
+    N = x.shape[0]
+    
+    # Reshape input to match forward pass computation
+    x_reshaped = x.reshape(N, -1)
+    
+    # Gradient w.r.t. weights: dw = x^T * dout
+    dw = x_reshaped.T.dot(dout)
+    
+    # Gradient w.r.t. bias: db = sum of dout over batch dimension
+    db = np.sum(dout, axis=0)
+    
+    # Gradient w.r.t. input: dx = dout * w^T, then reshape back to original shape
+    dx_reshaped = dout.dot(w.T)
+    dx = dx_reshaped.reshape(x.shape)
+
+    ########################################################################
+    #                           END OF YOUR CODE                           #
+    ########################################################################
+    return dx, dw, db
